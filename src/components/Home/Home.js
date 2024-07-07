81975c14c8eb364ae2c7c9366ea26207bbfffd42
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-07-07 19:18:18 +1000

    logo transition and some coloring

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import './Home.css';
 
+
 export default function Home() {
   return (
     <div id="Home" className="tabcontent">
@@ -10,6 +11,9 @@ export default function Home() {
           <source src="/img/home-vid.MP4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+          <div className="video-logo-container">
+            <img src="/img/stackpackers-logo.svg" alt="Logo" />
+          </div>
       </Container>
     </div>
   );
